b9b7744873d3b0c75ea6877280c6be22a1890f5f
mantovanig <gabriele.mantovani92@gmail.com>
2020-04-15 21:57:22 +0200

    FIX

--- src/lendbox-config.js
+++ src/lendbox-config.js
@@ -1,6 +1,6 @@
 export default {
   gitlabApi: "https://gitlab.com/api/v4",
-  privateToken: process.env.GITLAB_TOKEN,
+  privateToken: process.env.VUE_APP_GITLAB_TOKEN,
   maxAge: 168,
   membership: true,
   fetchCount: 50,
